ef9f9d543bf758284c07b3e25211d2854a12fc39
Pedro Nogueira <pnc@cin.ufpe.br>
2020-11-17 11:30:48 -0300

    v2.0

--- WebServer.py
+++ WebServer.py
@@ -2,34 +2,39 @@ import socket
 import sys
 import time
 import threading
+import os
+from datetime import datetime
+import zlib
 
 class WebServer(object):
 
-    def __init__(self, port=8080):
-        self.host = socket.gethostname()    
-        self.port = port
-        self.dir_arquivos = 'public'
+    def __init__(self, port=8080, serverSize = 10):
+        self.host = socket.gethostname() #salva o nome do host
+        self.port = port #salva a porta atual
+        self.dir_arquivos = 'public' #diretorio do arquivo
+        self.serverSize = serverSize #numero maximo de clientes que podem esperar
 
-    def start(self):
+    def run(self):
         self.soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        try:
+        try: #tenta ligar um socket ao server
             self.soc.bind((self.host, self.port))
             print("Server na porta {porta}.".format(porta=self.port))
-        except Exception as e:
+        except Exception as e: #se nao conseguir ele desliga o server
             print("Erro: nao foi possivel conectar-se a porta {porta}".format(porta=self.port))
             self.shutdown()
             sys.exit(1)
         
-        self._listen()
+        self.listen()
 
     
-    def _listen(self):
-        self.soc.listen(5)
+    def listen(self): #espera por clientes
+        self.soc.listen(self.serverSize)
         while True:
             (cliente, addr) = self.soc.accept()
             print("Cliente conectado com endereco", addr)
-            threading.Thread(target=self._recebe_cliente, args=(cliente, addr)).start()
+            #dedica uma thread para atender um cliente
+            threading.Thread(target=self.attend_cliente, args=(cliente, addr)).start()
 
     def shutdown(self):
         try:
@@ -39,9 +44,9 @@ class WebServer(object):
             print(e)
             pass
 
-    def _gera_headers(self, codigo_http, content_length):
+    def get_header(self, codigo_http, content_length, path = ''):
         header = 'HTTP/1.1 '
-
+        #indica se ocorreu erro ou nao
         if codigo_http == 200:
             header += '200 OK\r\n'
         elif codigo_http == 404:
@@ -50,30 +55,33 @@ class WebServer(object):
             header += 'HTTP Version Not Supported\r\n'
         elif codigo_http == 400:
             header += 'Bad Request\r\n'
-        
+        #completa o header
         header += 'Server: Servidor da galera\r\n'
         time_now = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
-        header += 'Date: {data}\r\n'.format(data=time_now)
+        header += 'Date: {data} GMT-3\r\n'.format(data=time_now)
         header += 'Content-Type: text/html\r\n'
         header += 'Content-Length {tam}\r\n'.format(tam=content_length)
+        if path:
+        	header += 'Last-Modified: ' + datetime.fromtimestamp(os.path.getmtime(self.dir_arquivos + path)).strftime("%a, %B %d, %Y %I:%M:%S") + ' GMT-3\r\n'
+
         header += 'Connection: close\r\n'
         header += '\r\n'
         return header
 
 
-    def _recebe_cliente(self, cliente, addr):
+    def attend_cliente(self, cliente, addr): # recebe os dados do cliente e retorna o que foi pedido
         TAM_PACOTE = 2048
 
         while True:
-            dados = cliente.recv(TAM_PACOTE).decode() 
+            dados = cliente.recv(TAM_PACOTE).decode()
 
             if not dados: 
-                res_header = self._gera_headers(400, 0)
-                res = res_header.encode("utf-8")
+                res_header = self.get_header(400, 0)
+                res = res_header.encode()
                 cliente.send(res)
                 cliente.close()
                 break
-            
+
             metodo_req = dados.split(' ')[0]
             print("Metodo:", metodo_req)
             print("Corpo:", dados)
@@ -81,46 +89,37 @@ class WebServer(object):
             if metodo_req == "GET" or metodo_req == "HEAD":
                 # checa versao solicitada 
                 versao = dados.split(' ')[2]
-                if versao != "HTTP/1.1":
-                    res_header = self._gera_headers(505, 0)
+                
+                arq_solicitado = dados.split(' ')[1]
+
+                # ignora parametro, caso haja
+                arq_solicitado = arq_solicitado.split('?')[0]
+                
+                # index.html como arquivo default
+                if arq_solicitado == "/":
+                    arq_solicitado = "/index.html"
+                
+                path_arquivo = self.dir_arquivos + arq_solicitado
+
+                # tenta abrir arquivos
+                try:
+                    f = open(path_arquivo, 'rb')
+                    if metodo_req == "GET":
+                        res_data = f.read().decode()
+                    f.close()
+                    tam = sys.getsizeof(res_data)
+                    res_header = self.get_header(200, tam)
+                
+                # caso não haja o arquivo requisitado, responde com 404
+                except Exception as e:
+                    res_header = self.get_header(404, 0)
                     res_data = ''
-                
-                else:
-                    arq_solicitado = dados.split(' ')[1]
 
-                    # ignora parametro, caso haja
-                    arq_solicitado = arq_solicitado.split('?')[0]
-                    
-                    # index.html como arquivo default
-                    if arq_solicitado == "/":
-                        arq_solicitado = "/index.html"
-                    
-                    path_arquivo = self.dir_arquivos + arq_solicitado
-
-                    # tenta abrir arquivos
-                    try:
-                        f = open(path_arquivo, 'r')
-                        if metodo_req == "GET":
-                            res_data = f.read()
-                        f.close()
-                        tam = sys.getsizeof(res_data)
-                        res_header = self._gera_headers(200, tam)
-                    
-                    # caso não haja o arquivo requisitado, responde com 404
-                    except Exception as e:
-                        if metodo_req == "GET":
-                            f = open(self.dir_arquivos+"404.html", 'rb')
-                            res_data = f.read()
-                            f.close()
-                        
-                        tam = sys.getsizeof(res_data)
-                        res_header = self._gera_headers(404, tam)
-
-                res = res_header.encode("utf-8")
+                res = res_header
                 if metodo_req == "GET":
-                    res += res_data.encode("utf-8")
+                    res += res_data
                 
-                cliente.send(res)
+                cliente.send(res.encode())
                 cliente.close()
                 break
             
--- server.py
+++ server.py
@@ -1,5 +1,15 @@
 import socket
 from WebServer import WebServer
+import sys
+import signal
 
-server = WebServer(8080)
-server.start()
+def shutdownServer(sig, unused):
+    #Fecha o server caso receba sinal
+    server.shutdown()
+    sys.exit(1)
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, shutdownServer)
+
+    server = WebServer(9999)
+    server.run()
